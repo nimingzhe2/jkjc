ee530b81965b110857c96fa91b798bc8e409e177
毛毛 <2681808375@qq.com>
2020-06-25 15:29:06 +0800

    用于无gui服务器

--- jkkkkk.py
+++ jkkkkk.py
@@ -2,17 +2,23 @@
 #将驱动放到python安装目录下的srict目录下，并且复制一份到谷歌浏览器的安装目录下
 #亦可以使用火狐浏览器，但也要下载相应版本的驱动，如上
 #学号 密码 温度为必填项目
-#如果爬脚本在服务器上没有执行可以设置一个邮箱提醒
+#如果害怕脚本在服务器上没有执行可以设置一个邮箱提醒
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 uname = 'Z18035536'  #####设置学号
 passwd = '123456'    #####设置密码
 Temperature = '36.8' #####设置温度（可以写一个列表，利用随机数索引传入保证每天的温度不一样，避免辅导员约茶）
 
-def jitb(uname,passwd,Temperature):
+def jitb(uname, passwd, Temperature):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')  #由于我使用的是centos无gui服务器 ，设置的无头模式
     start_url = 'http://hmgr.sec.lit.edu.cn/web/#/login'
-    brower = webdriver.Chrome("d:/guge/chromedriver.exe")  #调用谷歌浏览器（可以使用火狐，调用驱动的绝对路径）
+    # brower = webdriver.Chrome("d:/guge/chromedriver.exe")  #调用谷歌浏览器的驱动备注第二条所述的复制到的安装目录下（可以使用火狐，调用驱动的绝对路径）
+    brower = webdriver.Chrome(executable_path="./chromedriver", chrome_options=chrome_options) #调用谷歌浏览器的驱动备注第二条所述的复制到的安装目录下（可以使用火狐，调用驱动的绝对路径）
     brower.get(start_url)
     login_input = brower.find_element_by_xpath("//input[@placeholder='请输入密码']") #利用xpath获取选中框，键入，密码
     login_input.click()
@@ -21,7 +27,6 @@ def jitb(uname,passwd,Temperature):
     login_input = brower.find_element_by_xpath("//input[@placeholder='请输入账号']")#利用xpath选中用户框
     login_input.click()
     login_input.send_keys(uname)
-
     login_click = brower.find_element_by_xpath("//button[contains(@class,'van-button van-button--default')]") #登录
     login_click.click()
     time.sleep(2)
@@ -43,3 +48,5 @@ def jitb(uname,passwd,Temperature):
         print('已经填报过了，请明天再来')
     brower.quit()
 jitb(uname,passwd,Temperature)
+
+#作者:坏狗i
